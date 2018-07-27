94c45c45e40f950dda9f0b682b9b5dc8f6708864
Lynsey Powell <lynseympowell@gmail.com>
2018-07-26 19:11:03 -0600

    add testing

--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const { app, runServer, closeServer } = require('../server');
+const {app} = require('../server');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('server.js', function() {
+    it('should return status 200 and html', function() {
+
+        return chai.request(app)
+            .get('/')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.html;
+            });
+    });
+});
 
 
