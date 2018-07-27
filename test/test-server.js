5635ca5fbe052b276490becc3479d9144293c18a
Lynsey Powell <lynseympowell@gmail.com>
2018-07-26 18:40:37 -0600

    install dependences

---
+++ test/test-server.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, runServer, closeServer } = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+
