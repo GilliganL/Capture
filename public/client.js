d045d6474cbd4b8b9d5c8dfdc98ecb69f116a1ea
Lynsey Powell <lynseympowell@gmail.com>
2018-07-30 12:56:20 -0600

    add clientside static files with mock data

---
+++ public/client.js
@@ -0,0 +1,4 @@
+// const MOCK_LOGIN = 
+
+//Send login info, need mock data?
+
--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
+app.use('/feed', express.static('feedStatic'));
+app.use('/connections', express.static('connectionsStatic'));
+
 app.listen(process.env.PORT || 8080);
-
-
 
 module.exports = { app };
