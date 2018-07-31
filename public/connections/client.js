5c983d3e0ea855edc87e04995038d1ae2c81d986
Lynsey Powell <lynseympowell@gmail.com>
2018-07-31 17:09:32 -0600

    add client user  connections and search

--- public/connections/client.js
+++ public/connections/client.js
@@ -1,31 +1,26 @@
 const MOCK_USER_CONNECTIONS = {
     "userConnections" : [
         {
-            "id": "00001",
             "username": "Clinton.Bartell",
             "fullName":  "Clinton Bartell",
             "location": "Carolemouth, NC"
         },
         {
-            "id": "00002",
             "username": "Caleb.Murphy",
             "fullName": "Caleb Murphy",
             "location": "Gibsonmouth, MS"
         },
         {
-            "id": "00003",
             "username": "Kasandra.Schamberger",
             "fullName": "Kasandra Schamberger",
             "location": "Rohanton, IA"
         },
         {
-            "id": "00004",
             "username": "Eldon.Jerde",
             "fullName": "Eldon Jerde",
             "location": "Moenburgh, DE"
         },
         {
-            "id": "00005",
             "username": "Ottis.Reinger",
             "fullName": "Ottis Reinger",
             "location": "Jeffereyshire, MN"
@@ -33,14 +28,71 @@ const MOCK_USER_CONNECTIONS = {
     ]
 };
 
-function getUserConnections(callback) {
+const MOCK_SEARCH_CONNECTIONS = {
+    "userConnections" : [
+        {
+            "username": "Elizabeth.Bartell",
+            "fullName":  "Elizabeth Bartell",
+            "location": "Carolemouth, NC"
+        },
+        {
+            "username": "Caleb.Murphy",
+            "fullName": "Caleb Murphy",
+            "location": "Gibsonmouth, MS"
+        },
+        {
+            "username": "Liz.Smith",
+            "fullName": "Liz Schamberger",
+            "location": "Rohanton, IA"
+        },
+        {
+            "username": "Eliza.Jordan",
+            "fullName": "Eliza Jordan",
+            "location": "Moenburgh, DE"
+        },
+        {
+            "username": "Ottis.Reinger",
+            "fullName": "Ottis Reinger",
+            "location": "Jeffereyshire, MN"
+        }
+    ]
+};
+
+function listenForSearchButton() {
+    $('#searchForm').on('submit', event => {
+        event.preventDefault();
+        //turn user input into RegExp that checks for special characters also?
+        let searchTerm = new RegExp('Liz', 'i');
+        const resultsArray =  MOCK_SEARCH_CONNECTIONS.userConnections.filter(item => item.fullName.match(searchTerm));
+
+        getSearchResults(displaySearchResults, resultsArray);
+        
+    });
+}
+
+function getSearchResults(callback, data) {
+    //best place for listSection empty? Add getAndDisplay function?
+    $('.listSection').empty();
+    setTimeout(function(){callback(data)}, 1);
+}
+
+function displaySearchResults(data) {
+    for (index in data) {
+        $('.listSection').append(
+            `<div><p>${data[index].username}<br>
+            ${data[index].fullName}<br>
+            ${data[index].location}</p></div>`);
+    }
+}
+
+function getUserConnections(callback, data) {
     //setTimeOut for asynchronous call
-    setTimeout(function(){callback(MOCK_USER_CONNECTIONS)}, 1);
+    setTimeout(function(){callback(data)}, 1);
 }
 
 function displayUserConnections(data) {
     for (index in data.userConnections) {
-        $('body').append(
+        $('.listSection').append(
             `<div><p>${data.userConnections[index].username}<br>
             ${data.userConnections[index].fullName}<br>
             ${data.userConnections[index].location}</p></div>`);
@@ -48,9 +100,11 @@ function displayUserConnections(data) {
 }
 
 function getAndDisplayUserConnections() {
-    getUserConnections(displayUserConnections);
+    $('.listSection').empty();
+    getUserConnections(displayUserConnections, MOCK_USER_CONNECTIONS);
 }
 
 $(function() {
+    listenForSearchButton();
     getAndDisplayUserConnections();
-})
+});
