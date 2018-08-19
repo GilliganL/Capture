382bd66c691485e8885641a765f19331dadb0d32
Lynsey Powell <lynseympowell@gmail.com>
2018-08-19 15:49:34 -0600

    add styling

--- public/feed/client.js
+++ public/feed/client.js
@@ -1,32 +1,3 @@
-const MOCK_USER_FEED = {
-    "feedPosts" : [
-        {
-            "username": "Clinton.Bartell",
-            "image": "http://lorempixel.com/640/480/nightlife",
-            "caption": "Perspiciatis sequi esse harum." 
-        },
-        {
-            "username": "Caleb.Murphy",
-            "image": "http://lorempixel.com/640/480/people",
-            "caption": "Quo velit quasi corporis. Ducimus unde et laudantium quo consequuntur. Velit eveniet sed dolor cum. Aut id facere consequuntur." 
-        },
-        {
-            "username": "Kasandra.Schamberger",
-            "image": "http://lorempixel.com/640/480/nature",
-            "caption": "Error qui praesentium dolor ut beatae ipsam tempore perferendis. Quis odio sunt." 
-        },
-        {
-            "username": "Eldon.Jerde",
-            "image": "http://lorempixel.com/640/480/city",
-            "caption": "Consectetur quam suscipit consectetur provident ea facilis enim ut. Et pariatur cupiditate molestiae dolor numquam laboriosam ea tempore quo. Cupiditate et sapiente. Sapiente quam enim perspiciatis provident suscipit deserunt ea. Fugiat nam aliquam accusamus quibusdam nihil voluptate qui. Modi est ullam minima nulla autem." 
-        },
-        {
-            "username": "Ottis.Reinger",
-            "image": "http://lorempixel.com/640/480/food",
-            "caption": "Consequatur autem quo odio et rerum sequi aut vero sequi." 
-        }
-    ]
-};
 
 function listenForSubmitPost() {
     $('#postForm').on('submit', event => {
@@ -62,7 +33,7 @@ function getFeedPosts(callback) {
         url: '/api/feed/',
         headers: {
             'content-type': 'application/json',
-            // 'authorization': 'bearer' + localStorage.token
+            'authorization': 'bearer' + localStorage.token
         },
         type: 'GET'
     })
@@ -70,7 +41,7 @@ function getFeedPosts(callback) {
 }
 
 function displayFeedPosts(data) {
-//returned data, create hidden section to store userId for future searches?
+//returned data, create hidden section or variable to store userId for future searches?
     // caption    :"some caption"
     // created:"2018-08-18T23:25:21.742Z"
     // id : "5b71b5871080d01629dc0e97"
@@ -79,14 +50,30 @@ function displayFeedPosts(data) {
     // user : "Lynsey Powell"
   
     for (index in data) {
+        if(index % 2 == 0){
         $('.feedSection').append(
-            `<div class='postDiv'>
+            `<div class='postDiv flex-item'>
+                <div class='flex-item-hidden'></div>
+                <div class='flex-item-date'></div>
+                <div class='flex-item-content'>
                 <h3 class='poster'> ${data[index].user}</h3>
                 <p class='postCaption'>${data[index].caption}</p>
                 <img class='postImage' src="${data[index].image}">
-                <span
+                </div>
             </div>`);
-        //create variable with userId to use later to pull all posts by user?
+        } else {
+            $('.feedSection').append(
+                `<div class='postDiv flex-item flex-item-flipped'>
+                    <div class='flex-item-hidden'></div>
+                    <div class='flex-item-date'></div>
+                    <div class='flex-item-content'>
+                    <h3 class='poster'> ${data[index].user}</h3>
+                    <p class='postCaption'>${data[index].caption}</p>
+                    <img class='postImage' src="${data[index].image}">
+                    </div>
+                </div>`);
+
+        }
     }
 }
 
