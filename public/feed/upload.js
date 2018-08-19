0a9a077ad4d3cfbfc07acf7ed5862de192c0cf71
Lynsey Powell <lynseympowell@gmail.com>
2018-08-19 15:50:19 -0600

    add style files and images

---
+++ public/feed/upload.js
@@ -0,0 +1,48 @@
+
+
+
+// (() => {
+//     $('#postPhoto').on('change', event => { 
+//         const files = event.target.files;
+//         const file = files[0];
+//         if(file == null) {
+//             return alert('No file selected');
+//         }
+//         getSignedRequest(file);
+//     });
+// })();
+
+// function getSignedRequest(file) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `/api/feed/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status === 200){
+//                 const response = JSON.parse(xhr.responseText);
+//                 uploadFile(file, response.signedRequest, response.url);
+//             } else {
+//                 alert('Could not get signed URL');
+//             }
+//         }
+//     };
+//     xhr.send();
+// }
+
+// function uploadFile(file, signedRequest, url){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('PUT', signedRequest);
+//     xhr.onreadystatechange = () => {
+//         console.log(xhr.readyState);
+//         console.log(xhr.status);
+//         if(xhr.readyState === 4) {
+//             if(xhr.status === 200) {
+//                 $('#preview').src = url;
+//                 $('#save-url').value = url;
+//             } else {
+//                 alert('Could not upload file');
+//             }
+//         }
+//     };
+//     xhr.send(file);
+// }
+
